8a6af7eabf78cafd57378aa9ed889ca410ff3e66
infamous55 <infamouslegend555@gmail.com>
2024-10-12 17:51:37 +0300

    made ruff work with pyright

--- lua/infamous55/plugins/lspconfig.lua
+++ lua/infamous55/plugins/lspconfig.lua
@@ -90,10 +90,43 @@ function spec:config()
     lspconfig.bashls.setup({})
     lspconfig.graphql.setup({})
     lspconfig.clangd.setup({})
-    lspconfig.ruff.setup({})
     lspconfig.gopls.setup({})
     lspconfig.rust_analyzer.setup({})
     lspconfig.ts_ls.setup({})
+
+    lspconfig.ruff.setup({})
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup(
+            "lsp_attach_disable_ruff_hover",
+            { clear = true }
+        ),
+        callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client == nil then
+                return
+            end
+            if client.name == "ruff" then
+                -- Disable hover in favor of Pyright
+                client.server_capabilities.hoverProvider = false
+            end
+        end,
+        desc = "LSP: Disable hover capability from Ruff",
+    })
+    lspconfig.pyright.setup({
+        settings = {
+            pyright = {
+                -- Using Ruff's import organizer
+                disableOrganizeImports = true,
+            },
+            python = {
+                analysis = {
+                    -- Ignore all files for analysis to exclusively use Ruff for linting
+                    ignore = { "*" },
+                },
+            },
+        },
+    })
+
     lspconfig.elixirls.setup({
         cmd = {
             "/home/infamous55/.local/share/nvim/mason/packages/elixir-ls/language_server.sh",
